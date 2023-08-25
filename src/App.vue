713b3707c9363438f42c5079a6b66a4fbaaf1660
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-08-25 12:19:28 +0200

    Init : init the Vue composition API project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const header = ref("Shopping list App");
+  const items = ref([
+    {id:1, label: "poulet"},
+    {id:2, label: "ananas"},
+    {id:3, label: "Coca"},
+  ])
+</script>
+
+<template>
+  <main>
+    <h1 class="text-center">{{ header ? header: "Salut" }}</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id"> {{ item.label }}</li>
+    </ul>
+  </main>
+</template>
