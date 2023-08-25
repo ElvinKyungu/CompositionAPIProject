89073f62979c18428781f4b74a9770998b4dc64b
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-08-25 13:01:58 +0200

    fix : ajusting the same logic on priority method

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,20 @@
     {id:3, label: "Coca"},
   ]);
   const newItem = ref("");
-  const newItemPriority = ref('low')
+  const newItemHighPriority = ref(false)
 </script>
 
 <template>
   <main>
     <h1 class="text-center">{{ header }}</h1>
     <input type="text" placeholder="Add an items" v-model.trim="newItem">
-    {{ newItem }}
-    Priority :
     <label>
-      <input type="radio" v-model="newItemPriority" value="low">
-      Low
-    </label>
-    <label>
-      <input type="radio" v-model="newItemPriority" value="high">
-      High
+      <input type="checkbox" v-model="newItemHighPriority">
+      High Priority :
+      
     </label>
     <br>
-    {{ newItemPriority }}
+    {{ newItemHighPriority }}
     <ul>
       <li v-for="({id, label}, index) in items" :key="id"> 
         {{ index }}
