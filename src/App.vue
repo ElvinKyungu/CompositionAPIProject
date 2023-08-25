d92956047b440d01171dc91f31691f16a381a383
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-08-25 14:37:50 +0200

    feat : adding the new method saveItem

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,33 @@
   ]);
   const newItem = ref("");
   const newItemHighPriority = ref(false)
+  const saveItem = ()=>{
+    items.value.push({id: items.value.length +1, label: newItem.value})
+    newItem.value = ""
+  }
 </script>
 
 <template>
   <main>
     <h1 class="text-center">{{ header }}</h1>
-    <input type="text" placeholder="Add an items" v-model.trim="newItem">
-    <label>
-      <input type="checkbox" v-model="newItemHighPriority">
-      High Priority :
-      
-    </label>
+    <form @submit.prevent="saveItem">
+      <input 
+        type="text" 
+        placeholder="Add an items" 
+        v-model.trim="newItem"
+      >
+      <label>
+        <input 
+          type="checkbox" 
+          v-model="newItemHighPriority"
+        >
+        High Priority :
+      </label>
+      <button>
+        Ajouter 
+      </button>
+    </form>
     <br>
-    {{ newItemHighPriority }}
     <ul>
       <li v-for="({id, label}, index) in items" :key="id"> 
         {{ index }}
